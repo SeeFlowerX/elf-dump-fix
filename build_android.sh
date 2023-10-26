86fada937c489a351340b3b9e8e14260cf206ed3
SeeFlowerX <70688581+SeeFlowerX@users.noreply.github.com>
2023-10-26 10:25:18 +0800

    Delete build_android.sh

--- build_android.sh
+++
@@ -1,5 +0,0 @@
-export NDK_ROOT=/home/kali/Desktop/android-ndk-r25b
-export PATH=$NDK_ROOT/toolchains/llvm/prebuilt/linux-x86_64/bin:$PATH
-export PATH=$NDK_ROOT:$PATH
-
-ndk-build NDK_PROJECT_PATH=. NDK_APPLICATION_MK=./Application.mk APP_BUILD_SCRIPT=./Android.mk
